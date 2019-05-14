71de1aafddbc41a2230b219faa1729ba2c004917
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-14 17:50:35 +0000

    completes ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_all = Store.sum(:annual_revenue)
+
+# pp @total_all
+
+@average_all = Store.average(:annual_revenue)
+
+# pp @average_all.round
+
+@million_all = Store.where('(annual_revenue < ?)', 1000000).count
+
+# pp @million_all
+
