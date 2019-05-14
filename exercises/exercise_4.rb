01b7f032b3f011d2f8737c58c0ac73c29fe286e6
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-14 17:34:59 +0000

    completes ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey_store = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler_store = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown_store = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# for store in @mens_stores do
+#   puts store.name
+#   puts store.annual_revenue
+# end
+
+@womens_stores = Store.where('(womens_apparel = ?) AND (annual_revenue < ?)', true, 1000000)
+
+# pp @womens_stores
