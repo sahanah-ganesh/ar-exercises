5ae8f8356b338316051beea6b5fd8523fc325487
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-14 17:12:10 +0000

    finishes exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,4 +9,4 @@ burnaby_store = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_appar
 richmond_store = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 gastown_store = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts Store.count
+# pp Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: 'Sahanah')
+
+# pp @store1
+# pp @store2
+# pp @store1.name
